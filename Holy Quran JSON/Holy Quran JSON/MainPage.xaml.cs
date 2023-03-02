3d44d2c9bfbcfd9f66dcdaab276dbcdfe6c782ce
Abed <abed.jaber81@gmail.com>
2023-03-02 09:36:00 +0300

    edit 3

--- Holy Quran JSON/Holy Quran JSON/MainPage.xaml.cs
+++ Holy Quran JSON/Holy Quran JSON/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.Diagnostics;
 
 namespace Holy_Quran_JSON;
 public partial class MainPage : ContentPage
@@ -11,7 +12,17 @@ public partial class MainPage : ContentPage
     }
     public async void deserialize()
     {
-        using var stream = await FileSystem.OpenAppPackageFileAsync("Quran.json");
+        //using var stream = await FileSystem.OpenAppPackageFileAsync("Quran.json");
+        string txt;
+        var watch = new System.Diagnostics.Stopwatch();
+        watch.Start();
+        string mainDir = FileSystem.Current.AppDataDirectory;
+        var fullpath = Path.Combine(mainDir, "Quran.json");
+        Stream fileStream = await FileSystem.Current.OpenAppPackageFileAsync("Quran.json");
+        StreamReader reader = new StreamReader(fileStream);
+        txt = reader.ReadToEnd();
+        roots = JsonConvert.DeserializeObject<List<Root>>(txt);
+        watch.Stop();
         int i = 0;
 
     }
