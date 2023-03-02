caaea728e7648fbaa47eeaa4f5ad5fd82125c35a
AbedJaberB <AbedJaberB@ADMIN>
2023-03-02 06:59:56 +0300

    edit 2

--- Holy Quran JSON/Holy Quran JSON/MainPage.xaml.cs
+++ Holy Quran JSON/Holy Quran JSON/MainPage.xaml.cs
@@ -1,63 +1,15 @@
-﻿namespace Holy_Quran_JSON;
+﻿using Newtonsoft.Json;
 
+namespace Holy_Quran_JSON;
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
-
-    /*
-	 * 
-	 * // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
-    public class Array
+    List<Root> roots= new List<Root>();
+    public MainPage()
     {
-        public int id { get; set; }
-        public string ar { get; set; }
-        public string en { get; set; }
-        public string filename { get; set; }
-        public string path { get; set; }
-        public string dir { get; set; }
-        public int size { get; set; }
+        InitializeComponent();
     }
-
-    public class Root
+    public void deserialize()
     {
-        public int id { get; set; }
-        public string name { get; set; }
-        public string name_en { get; set; }
-        public string name_translation { get; set; }
-        public int words { get; set; }
-        public int letters { get; set; }
-        public string type { get; set; }
-        public string type_en { get; set; }
-        public string ar { get; set; }
-        public string en { get; set; }
-        public List<Array> array { get; set; }
+        
     }
-
-
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * */
-
-
 }
-
---
+++ Holy Quran JSON/Holy Quran JSON/Root.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Holy_Quran_JSON
+{
+    public class Root
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string name_en { get; set; }
+        public string name_translation { get; set; }
+        public int words { get; set; }
+        public int letters { get; set; }
+        public string type { get; set; }
+        public string type_en { get; set; }
+        public string ar { get; set; }
+        public string en { get; set; }
+        public List<Array> array { get; set; }
+    }
+    public class Array
+    {
+        public int id { get; set; }
+        public string ar { get; set; }
+        public string en { get; set; }
+        public string filename { get; set; }
+        public string path { get; set; }
+        public string dir { get; set; }
+        public int size { get; set; }
+    }
+}
